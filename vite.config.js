f3105a39e693f3ded8f67898587794fa04898cc6
kprakash18 <iamkprakash18@gmail.com>
2026-07-28 14:37:40 +0530

    setup: make imports easy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from 'vite' ;
+import react from '@vitejs/plugin-react' ;
+import tailwindcss from '@tailwindcss/vite' ;
+import { resolve, dirname} from '@tailwindcss/vite' ;
+import { fileURLToPath } from 'url' ;
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  resolve: {
+    alias : {
+      '#components' : resolve(dirname(fileURLToPath(import.meta.url)), 'components') ,
+      '#constants' : resolve(dirname(fileURLToPath(import.meta.url)), 'constants') ,
+      '#store' : resolve(dirname(fileURLToPath(import.meta.url)), 'store') ,
+      '#hoc' : resolve(dirname(fileURLToPath(import.meta.url)), 'hoc') ,
+      '#windows' : resolve(dirname(fileURLToPath(import.meta.url)), 'windows') ,
+    }
+  }
 })
